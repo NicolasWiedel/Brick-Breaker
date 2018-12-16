ef605da4cc76f9d90d3a9e0cfbe8e9972dac2bed
NICOLASWIEDEL\Nicolas Wiedel <nicolaswiedel@googlemail.com>
2018-12-16 21:23:16 +0100

    2. creating project

--- core/src/com/jga/brickbreaker/screen/game/GameController.java
+++ core/src/com/jga/brickbreaker/screen/game/GameController.java
@@ -1,4 +1,8 @@
 package com.jga.brickbreaker.screen.game;
 
 public class GameController {
+
+    // == public methoods =
+    public void update(float delta) {
+    }
 }
--- core/src/com/jga/brickbreaker/screen/game/GameScreen.java
+++ core/src/com/jga/brickbreaker/screen/game/GameScreen.java
@@ -1,15 +1,53 @@
 package com.jga.brickbreaker.screen.game;
 
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.jga.util.game.GameBase;
 
 public class GameScreen extends ScreenAdapter {
 
     // == attributes ==
     private final GameBase game;
+    private final AssetManager assetManager;
+
+    private GameController controller;
+    private GameRenderer renderer;
+    private SpriteBatch batch;
 
     // == constructor ==
     public GameScreen(GameBase game){
         this.game = game;
+        assetManager = game.getAssetManager();
+        batch = game.getBatch();
+    }
+
+    // == public methods
+
+    @Override
+    public void show() {
+        controller = new GameController();
+        renderer = new GameRenderer(controller, batch, assetManager);
+
+    }
+    @Override
+    public void render(float delta) {
+        controller.update(delta);
+        renderer.render(delta);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        renderer.resize(width, height);
+    }
+
+    @Override
+    public void hide() {
+        dispose();
+    }
+
+    @Override
+    public void dispose() {
+        renderer.dispose();
     }
 }
